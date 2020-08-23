dba9c6346cd62dcea474b07202c4354ecdefe314
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2020-08-23 14:04:56 -0300

    Adding minor changes to the header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header() {
+    return(
+        <div className="header">
+            <nav className="header-nav">
+                <ul>
+                    <li>Home</li>
+                    <li>Who I Am</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
